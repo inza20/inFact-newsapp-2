bdf980a18144fbb1a8f1740fd8801a747ff51a4a
Inshirah Zainab <inshirahzainab@gmail.com>
2023-01-28 12:54:46 +0530

    Added path to category : "general"

--- my-news-app/src/App.js
+++ my-news-app/src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
         <div>
           <Navbar />
           <Routes>
-            {/* <Route exact path='/' element={<News key='general' pageSize={this.pageSize} country='us' category='general' />}></Route> */}
+            <Route exact path='/' element={<News key='general' pageSize={this.pageSize} country='us' category='general' />}></Route>
             <Route exact path='/business' element={<News key='business' pageSize={this.pageSize} country='us' category='business' />}></Route>
             <Route exact path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country='us' category='entertainment' />}></Route>
             <Route exact path='/health' element={<News key='health' pageSize={this.pageSize} country='us' category='health' />}></Route>
